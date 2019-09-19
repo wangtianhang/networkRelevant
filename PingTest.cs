cebc5139c8ccdbd991f21f60c95025b6c184675b
wth <690879430@qq.com>
2019-09-19 20:32:11 +0800

    添加pingtest

---
+++ PingTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class PingTest
+{
+    static void Main(string[] args)
+    {
+        System.Net.NetworkInformation.Ping ping = new System.Net.NetworkInformation.Ping();
+        System.Net.NetworkInformation.PingReply reply = ping.Send("baidu.com", 3000, new byte[1473]);
+        Console.WriteLine(reply.Status.ToString());
+        Console.ReadLine();
+    }
+}
